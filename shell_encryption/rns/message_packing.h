259ce90fdd021d035d237be3dce5ec5a17ff0367
Phillipp Schoppmann <schoppmann@google.com>
2025-10-10 13:20:57 -0400

    Update dependencies and fix build

--- shell_encryption/rns/message_packing.h
+++ shell_encryption/rns/message_packing.h
@@ -16,93 +16,127 @@
 #ifndef RLWE_RNS_MESSAGE_PACKING_H_
 #define RLWE_RNS_MESSAGE_PACKING_H_
 
+#include <algorithm>
 #include <vector>
+
+#include "absl/types/span.h"
 
 namespace rlwe {
 
-// Packs integers in [0, `packing_base`) from `messages` into a smaller number
-// of larger integers in [0, `packing_base`^`num_packing`). Groups the packed
-// messages into vectors with at most `num_coeffs` elements. If the number of
-// messages is not a multiple of `num_packing`, we pad with zeros.
-// Expects packing_base > 1, num_packing > 0, num_coeffs > 0,
-// packing_base^num_packing < std::numeric_limits<BigInteger>::max().
-template <typename Integer, typename BigInteger>
-std::vector<std::vector<BigInteger>> PackMessages(
-    const std::vector<Integer>& messages, Integer packing_base, int num_packing,
-    int num_coeffs) {
-  std::vector<std::vector<BigInteger>> all_packed_messages;
-  int max_coeffs = messages.size() / num_packing + 1;
-  int max_poly = max_coeffs / num_coeffs + 1;
-  all_packed_messages.reserve(max_poly);
+namespace internal {
 
+// Returns ceil(x / y).
+template <typename T>
+T DivideAndRoundUp(const T& x, const T& y) {
+  return (x + y - 1) / y;
+}
+
+}  // namespace internal
+
+// Packs integers in [0, `packing_base`) from `messages` into a smaller number
+// of larger integers in [0, `packing_base`^`packing_dimension`). Expects
+// packing_base > 1, packing_dimension > 0, and
+// packing_base^packing_dimension < std::numeric_limits<BigInteger>::max().
+template <typename Integer, typename BigInteger>
+std::vector<BigInteger> PackMessagesFlat(absl::Span<const Integer> messages,
+                                         Integer packing_base,
+                                         int packing_dimension) {
+  int num_packed_coeffs =
+      internal::DivideAndRoundUp<int>(messages.size(), packing_dimension);
   std::vector<BigInteger> packed_messages;
-  packed_messages.reserve(num_coeffs);
+  packed_messages.reserve(num_packed_coeffs);
+
   // Each packed_message is a base-packing_base value with messages as digits,
-  // i.e. packed_message = sum(messages[k * j + i] * B^(k - 1 - i), i=0..k-1),
-  // for k = num_packing.
+  // i.e. packed_message = sum(messages[k * j + i] * B^i, i=0..k-1),
+  // for k = packing_dimension.
   BigInteger packed_message = 0;
+  BigInteger multiplier = 1;
   int packing_idx = 0;
   for (int i = 0; i < messages.size(); ++i) {
     Integer x = messages[i];
-    packed_message *= packing_base;
-    packed_message += x;
+    packed_message += x * multiplier;
+    multiplier *= packing_base;
     packing_idx++;
 
-    // Pad with zeros if we reached the end of input
-    if (i == messages.size() - 1) {
-      while (packing_idx < num_packing) {
-        packed_message *= packing_base;
-        packing_idx++;
-      }
-    }
-
     // Packed enough messages on an integer
-    if (packing_idx == num_packing) {
+    if (packing_idx == packing_dimension || i == messages.size() - 1) {
       packed_messages.push_back(packed_message);
       packed_message = 0;
+      multiplier = 1;
       packing_idx = 0;
     }
+  }
+  return packed_messages;
+}
 
-    // Stored enough packed_messages in a vector, or reached the end of input
-    if (packed_messages.size() == num_coeffs || i == messages.size() - 1) {
-      all_packed_messages.push_back(std::move(packed_messages));
-      packed_messages.clear();
+// Unpacks large integers from [0, `packing_base`^`packing_dimension`) into
+// smaller integers, by decomposing them on base `packing_base`. If the number
+// of messages is not a multiple of `packing_dimension`, we pad with zeros.
+// Expects packing_base > 1, packing_dimension > 0, and
+// packing_base^packing_dimension < std::numeric_limits<BigInteger>::max().
+template <typename Integer, typename BigInteger>
+std::vector<Integer> UnpackMessagesFlat(
+    absl::Span<const BigInteger> packed_messages, Integer packing_base,
+    int packing_dimension) {
+  std::vector<Integer> unpacked_messages;
+  unpacked_messages.reserve(packing_dimension * packed_messages.size());
+
+  for (BigInteger packed_message : packed_messages) {
+    // Decompose `packed_message` on base `packing_base`
+    for (int k = 0; k < packing_dimension; ++k) {
+      unpacked_messages.push_back(
+          static_cast<Integer>(packed_message % packing_base));
+      packed_message /= packing_base;
     }
+  }
+  return unpacked_messages;
+}
+
+// Packs integers in [0, `packing_base`) from `messages` into a smaller number
+// of larger integers in [0, `packing_base`^`packing_dimension`). Groups the
+// packed messages into vectors with at most `num_coeffs_per_packed_vector`
+// elements. If the number of messages is not a multiple of `packing_dimension`,
+// we pad with zeros. Expects packing_base > 1, packing_dimension > 0,
+// num_coeffs_per_packed_vector > 0, and packing_base^packing_dimension <
+// std::numeric_limits<BigInteger>::max().
+template <typename Integer, typename BigInteger>
+std::vector<std::vector<BigInteger>> PackMessages(
+    absl::Span<const Integer> messages, Integer packing_base,
+    int packing_dimension, int num_coeffs_per_packed_vector) {
+  std::vector<BigInteger> packed_messages =
+      PackMessagesFlat<Integer, BigInteger>(messages, packing_base,
+                                            packing_dimension);
+  int num_packed_coeffs = packed_messages.size();
+  int num_packed_vectors = internal::DivideAndRoundUp<int>(
+      num_packed_coeffs, num_coeffs_per_packed_vector);
+  std::vector<std::vector<BigInteger>> all_packed_messages(num_packed_vectors);
+  auto packed_messages_it = packed_messages.begin();
+  for (int i = 0; i < num_packed_vectors;
+       ++i, packed_messages_it += num_coeffs_per_packed_vector) {
+    all_packed_messages[i].resize(num_coeffs_per_packed_vector, 0);
+    std::copy_n(packed_messages_it,
+                std::min(num_coeffs_per_packed_vector, num_packed_coeffs),
+                all_packed_messages[i].begin());
+    num_packed_coeffs -= num_coeffs_per_packed_vector;
   }
   return all_packed_messages;
 }
 
-// Unpacks large integers from [0, `packing_base`^`num_packing`) into smaller
-// integers, by decomposing them on base `packing_base`.
-// Expects packing_base > 1, num_packing > 0, packing_base^num_packing <
+// Unpacks large integers from [0, `packing_base`^`packing_dimension`) into
+// smaller integers, by decomposing them on base `packing_base`. Expects
+// packing_base > 1, packing_dimension > 0, packing_base^packing_dimension <
 // std::numeric_limits<BigInteger>::max().
 template <typename Integer, typename BigInteger>
 std::vector<Integer> UnpackMessages(
     const std::vector<std::vector<BigInteger>>& all_packed_messages,
-    Integer packing_base, int num_packing) {
-  std::vector<Integer> all_unpacked_messages;
-  std::vector<Integer> unpacked_messages;
-  unpacked_messages.reserve(num_packing);
-
-  for (auto& packed_messages : all_packed_messages) {
-    for (BigInteger packed_message : packed_messages) {
-      // Decompose `packed_message` on base `packing_base`
-      for (int k = 0; k < num_packing; ++k) {
-        BigInteger quot = packed_message / packing_base;
-        Integer rem = static_cast<Integer>(packed_message % packing_base);
-        unpacked_messages.push_back(rem);
-        packed_message = quot;
-      }
-
-      // Reverse order because we packed the first message in the most
-      // significant position
-      for (int k = num_packing - 1; k >= 0; --k) {
-        all_unpacked_messages.push_back(unpacked_messages[k]);
-      }
-      unpacked_messages.clear();
-    }
+    Integer packing_base, int packing_dimension) {
+  std::vector<BigInteger> packed_messages;
+  for (const auto& packed_vector : all_packed_messages) {
+    packed_messages.insert(packed_messages.end(), packed_vector.begin(),
+                           packed_vector.end());
   }
-  return all_unpacked_messages;
+  return UnpackMessagesFlat<Integer, BigInteger>(packed_messages, packing_base,
+                                                 packing_dimension);
 }
 
 }  // namespace rlwe
